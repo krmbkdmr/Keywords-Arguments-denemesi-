059cc80a239683de0f8389cc55376f4a6e296a42
Kerem Bekdemir <kbekdemir5548@gmail.com>
2026-09-13 00:12:38 +0300

    Yaşın 18'e göre kıyaslaması ve ona göre işlem devamı olup olmayacağıeklendi.

--- argsandkwargs.py
+++ argsandkwargs.py
@@ -5,6 +5,9 @@ def kwarguments (**kwargument):
 	print(f"Soyisminiz:{kwargument["soyisim"]}")
 	print(f"Yasiniz:{kwargument["yas"]}")
 
+	if kwargument["yas"] < 18:
+		print("Yaşınız 18'den küçük olduğu için maalesef devam edemiyorsunuz!")
+
 İsim = input("İsminiz Nedir?")
 Soyisim = input("Soyisminiz Nedir?")
 Yas = int(input("Yasiniz Nedir?"))
